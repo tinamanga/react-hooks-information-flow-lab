41bea7a9a4a6b05abff2943d3e7c0122092b5a84
tinamanga <tinamanga@gmail.com>
2025-04-14 23:43:09 +0300

    using callback functions to update state in a parent component and move state based on which components need access

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header";
+
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
-  function onDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+  function handleDarkModeClick() {
+    setIsDarkMode((prevMode) => !prevMode);
   }
 
+
+  const appClass = isDarkMode ? "App dark" : "App light";
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+    <div className={appClass}>
+      <Header onDarkModeClick={handleDarkModeClick}/>
       <ShoppingList items={itemData} />
     </div>
   );
---
+++ src/components/Filter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Filter({ selectedCategory, onCategoryChange }) {
+  function handleChange(e) {
+    onCategoryChange(e.target.value);
+  }
+
+  return (
+    <div className="Filter">
+      <select name="filter" value={selectedCategory} onChange={handleChange}>
+        <option value="All">All</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
+    </div>
+  );
+}
+
+export default Filter;
